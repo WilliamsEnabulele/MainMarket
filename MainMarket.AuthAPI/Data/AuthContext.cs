1f2b3fcd3d57f2d03fa82b59b2d4ceade50afd94
Williams Enabulele <enabsdigital@gmail.com>
2024-02-18 10:27:19 +0100

    add auth api db context

---
+++ MainMarket.AuthAPI/Data/AuthContext.cs
@@ -0,0 +1,18 @@
+﻿using MainMarket.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainMarket.AuthAPI.Data;
+
+public class AuthContext : IdentityDbContext<AppUser>
+{
+    public AuthContext(DbContextOptions<AuthContext> options): base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
