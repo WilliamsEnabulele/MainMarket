21650d32f109fc9af6caba5d6114a9e7f5098a14
Williams Enabulele <enabsdigital@gmail.com>
2024-02-18 03:17:23 +0100

    Create views and controllers

--- MainMarket.Web/Controllers/CouponController.cs
+++ MainMarket.Web/Controllers/CouponController.cs
@@ -19,8 +19,95 @@ public class CouponController : Controller
         var response = await _couponService.GetAllCouponsAsync();
         if (response != null && response.Succeeded)
         {
-            couponList.Add(response.Data);
+            couponList= response.Data;
+        }
+        else
+        {
+            TempData["error"] = response.Errors;
         }
         return View(couponList);
+    }
+
+    public async Task<IActionResult> UpdateCoupon(string couponId)
+    {
+        var response = await _couponService.GetCouponByIdAsync(couponId);
+        if (response != null && response.Succeeded)
+        {
+            return View(response.Data);
+        }
+
+        return NotFound();
+    }
+
+    public async Task<IActionResult> DeleteCoupon(string couponId)
+    {
+        var response = await _couponService.GetCouponByIdAsync(couponId);
+
+        if (response != null && response.Succeeded)
+        {
+            return View(response.Data);
+        }
+
+        return NotFound();
+    }
+
+    public IActionResult CreateCoupon()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCoupon(CouponDto couponDto)
+    {      
+        if (ModelState.IsValid) {
+            var response = await _couponService.CreateCouponAsync(couponDto);
+
+            if (response.Succeeded)
+            {
+                TempData["success"] = "Coupon successfully created";
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            else
+            {
+                TempData["error"] = response?.Errors;
+            }
+        }
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CouponUpdate(CouponDto couponDto)
+    {
+        var response = await _couponService.UpdateCouponAsync(couponDto);
+        if (response != null && response.Succeeded)
+        {
+            TempData["success"] = "Coupon successfully updated";
+            return RedirectToAction(nameof(CouponIndex));
+        }
+        else
+        {
+            TempData["error"] = response?.Errors;
+        }
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CouponDelete(string couponId)
+    {
+        var response = await _couponService.DeleteCouponAsync(couponId);
+
+        if (response != null && response.Succeeded)
+        {
+            TempData["success"] = "Coupon successfully deleted";
+            return RedirectToAction(nameof(CouponIndex));
+        }
+        else
+        {
+            TempData["error"] = response?.Errors;
+        }
+
+        return View();
     }
 }
