4cc459e6ee77aec5eb0e68f6a0a83c7a2490530c
Williams Enabulele <enabsdigital@gmail.com>
2024-02-18 10:26:16 +0100

    init auth api project

--- MainMarket.AuthAPI/Program.cs
+++ MainMarket.AuthAPI/Program.cs
@@ -1,12 +1,9 @@
+using MainMarket.AuthAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,7 +14,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
