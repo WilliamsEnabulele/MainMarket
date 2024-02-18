c908aa826fe6f3d6b756d65977ddd3868044d729
Williams Enabulele <enabsdigital@gmail.com>
2024-02-18 10:29:37 +0100

    set up app user and deps

---
+++ MainMarket.AuthAPI/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using MainMarket.AuthAPI.Data;
+using MainMarket.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace MainMarket.AuthAPI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add Fluent Validation
+        AssemblyScanner.FindValidatorsInAssembly(Assembly.GetExecutingAssembly())
+            .ForEach(result => services.AddScoped(result.InterfaceType, result.ValidatorType));
+
+        // Disable Model State Validation
+        services.Configure<ApiBehaviorOptions>(opts =>
+        {
+            opts.SuppressModelStateInvalidFilter = true;
+        });
+
+        // Add DbContext
+        services.AddDbContext<AuthContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        services.AddIdentity<AppUser, IdentityRole>()
+            .AddEntityFrameworkStores<AuthContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
